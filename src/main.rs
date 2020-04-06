d0b41806001da1d6609e00b4df3822b163dfc925
Adam Nemecek <adamnemecek@gmail.com>
2020-04-06 16:43:00 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub struct Vec1 {
 impl Vec1 {
     pub fn new() -> Self {
         Self {
-            data: vec![0,1,2,3,4,5]
+            data: vec![1,2,3,4]
         }
     }
 }
@@ -24,26 +24,42 @@ impl std::ops::DerefMut for Vec1 {
     }
 }
 
-pub struct IntoIter {
-    inner: Vec1
-}
+// pub struct IntoIter {
+//     idx: usize,
+//     inner: Vec1
+// }
 
-impl Iterator for IntoIter {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
+// impl Iterator for IntoIter {
+//     type Item = u32;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if self.idx == self.inner.len() {
+//             None
+//         }
+//         else {
+//             let result = self.inner[self.idx];
+//             self.idx += 1;
+//             Some(result)
+//         }
+//     }
+// }
 
-impl IntoIterator for Vec1 {
-    type Item = u32;
-    type IntoIter = IntoIter;
-    fn into_iter(self) -> Self::IntoIter {
-        todo!()
-    }
+// impl IntoIterator for Vec1 {
+//     type Item = u32;
+//     type IntoIter = IntoIter;
+//     fn into_iter(self) -> Self::IntoIter {
+//         todo!()
+//     }
 
-}
+// }
 
 fn main() {
-    println!("Hello, world!");
+    let v1 = Vec1::new();
+    let mut v1_iter = v1.iter();
+
+    // iter() returns an iterator of slices.
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    // assert_eq!(v1_iter.next(), Some(&4));
+    println!("{}", v1_iter.next().unwrap());
 }
